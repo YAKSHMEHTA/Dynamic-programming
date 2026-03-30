97a0ccf0f86267871c95966e4cfdccf0cb228b2f
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-03-30 21:58:56 +0530

    Et init

---
+++ LCS/EvaluateTrue.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int eva
+
+int main(){ 
+    vector<int> arr = {10, 20, 30, 40, 50};
+    int n = arr.size();
+    for(int i=1;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    
+    return 0;
+}
